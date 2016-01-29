6f0dc8fae47c0edf915523cc97c135e2de95c59d
Tim Wagner <t.wagner@techdivision.com>
2016-01-29 16:05:48 +0100

    Add PHPUnit tests for AbstractNode and AbstractValueNode classes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+# Version 2.0.1
+
+## Bugfixes
+
+* None
+
+## Features
+
+* Add PHPUnit tests for AbstractNode and AbstractValueNode classes
+
 # Version 2.0.0
 
 ## Bugfixes
---
+++ tests/AppserverIo/Description/Api/Node/AbstractValueNodeTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * AppserverIo\Appserver\Core\Api\Node\AbstractValueNodeTest
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io/appserver
+ * @link      http://www.appserver.io
+ */
+
+namespace AppserverIo\Description\Api\Node;
+
+/**
+ * Test for the abstract value node implementation.
+ *
+ * @author    Tim Wagner <tw@appserver.io>
+ * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      https://github.com/appserver-io/appserver
+ * @link      http://www.appserver.io
+ */
+class AbstractValueNodeTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * The mock instance of the abstract class.
+     *
+     * @var \AppserverIo\Appserver\Core\Api\Node\AppstractValueNode
+     */
+    protected $abstractValueNode;
+
+    /**
+     * Initializes an instance of the abstract class we want to test.
+     *
+     * @return void
+     * @see PHPUnit_Framework_TestCase::setUp()
+     */
+    protected function setUp()
+    {
+        $this->abstractValueNode = $this->getMockForAbstractClass('AppserverIo\Description\Api\Node\AbstractValueNode');
+    }
+
+    /**
+     * Tests the setter/getter for the value node.
+     *
+     * @return void
+     */
+    public function testSetGetNodeValue()
+    {
+        $this->abstractValueNode->setNodeValue($nodeValue = new NodeValue());
+        $this->assertSame($nodeValue, $this->abstractValueNode->getNodeValue());
+    }
+}
